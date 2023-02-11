1c6a5690c965652420239cbe35b88731ed02e8ff
Stevixent <chiadikavirtue@gmail.com>
2023-02-11 21:21:06 +0100

    ok

---
+++ main.js
@@ -0,0 +1,103 @@
+const form =  document.getElementById('form');
+const username = document.getElementById('username');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    Validate();
+})
+
+
+const isEmail = (emailVal) => {
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 2)return false;
+    if(dot === emailVal.length -1) return false;
+    return true;
+}
+
+function Validate() {
+    const usernameVal = username.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+
+
+    //username
+    if (usernameVal === "") {
+        setErrorMsg(username, 'first name cannot be blank');
+    }
+    else if(usernameVal.length <= 2){
+        setErrorMsg(username, 'min 3 char');
+    }
+    else{
+        setSuccessMsg(username); 
+    }
+
+    //lastname
+    if (lastnameVal === "") {
+        setErrorMsg(lastname, 'last name cannot be blank');
+    }
+    else if(lastnameVal.length <= 2){
+        setErrorMsg(lastname, 'min 3 char');
+    }
+    else{
+        setSuccessMsg(lastname); 
+    }
+
+    //email
+    if (emailVal === "") {
+        setErrorMsg(email, 'email cannot be blank');
+    }
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
+    }
+    else{
+        setSuccessMsg(email); 
+    }
+
+    //password
+    if (passwordVal === "") {
+        setErrorMsg(password, 'password cannot be blank');
+    }
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
+    }
+    else{
+        setSuccessMsg(password); 
+    }
+
+    //confirm password
+    //password
+    if (cpasswordVal === "") {
+        setErrorMsg(cpassword, 'password cannot be blank');
+    }
+    else if(passwordVal != cpasswordVal){
+        setErrorMsg(cpassword, 'Not Matched!');
+    }
+    else{
+        setSuccessMsg(cpassword); 
+    }
+    SuccessMsg(usernameVal);
+
+}
+
+
+function setErrorMsg(input, errormsgs) {
+    const formcontrol = input.parentElement;
+    const small = formcontrol.querySelector('small');
+    formcontrol.className = "form-control error";
+    small.innerText = errormsgs;
+}
+
+function setSuccessMsg(input) {
+    const formcontrol = input.parentElement;
+    formcontrol.className = "form-control success";
+
+}
